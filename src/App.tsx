f18e0e4952648425aa0d986984cd2442a357a0f1
Melissa Astbury <melissajastbury@gmail.com>
2020-04-28 21:33:00 +0100

    implemented adding items and removing items from the array to show in seperate divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(["nails", "hair"]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    setTodos([...todos, input]);
+  };
+
+  const minusTodo = (i: number) => {
+    let newTodos = todos.filter((item, index) => {
+      return index !== i;
+    });
+    setTodos(newTodos);
+  };
+
+  const onInputChange = (event: any) => {
+    setInput(event.target.value);
+  };
+
   return (
-    <div>
-      <p>hi</p>
+    <div className="app">
+      <div className="todos">
+        {todos.map((item, index) => {
+          return (
+            <div key={index} className="todo-item">
+              <p>
+                {item}
+                <span
+                  onClick={() => {
+                    minusTodo(index);
+                  }}
+                >
+                  -
+                </span>
+                <span
+                  onClick={() => {
+                    minusTodo(index);
+                  }}
+                >
+                  edit
+                </span>
+              </p>
+            </div>
+          );
+        })}
+        <input type="text" name="todo" value={input} onChange={onInputChange} />
+        <button onClick={addTodo}>+</button>
+      </div>
     </div>
   );
 };
